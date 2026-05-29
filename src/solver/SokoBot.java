84ebc25568add29f680588cacd499a2174299de1
John <john_william_go@dlsu.edu.ph>
2026-05-29 13:37:57 +0800

    first commit

---
+++ src/solver/SokoBot.java
@@ -0,0 +1,56 @@
+package solver;
+
+public class SokoBot {
+    String output;
+  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+    /*
+     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
+     */
+    /*
+     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
+     * sequence
+     * that just moves left and right repeatedly.
+     */
+    try {
+      Thread.sleep(3000);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    up();
+    down();
+    left();
+    right();
+    up();
+    down();
+    left();
+    right();
+    up();
+    down();
+    left();
+    right();
+
+
+    return output;
+  }
+
+  public void up(){
+      output += "u";
+  }
+
+  public void down(){
+      output += "d";
+  }
+
+  public void left(){
+      output += "l";
+  }
+
+  public void right(){
+      output += "r";
+  }
+
+
+
+
+}
