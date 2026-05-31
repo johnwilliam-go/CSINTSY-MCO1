41299351ee1a4e6c2438d0682bdffd57f319a10b
John <john_william_go@dlsu.edu.ph>
2026-05-31 14:15:47 +0800

    Added the ff: 1. Displaying the coordinates of the box, goal, and player 2. move randomizer (this is to test the deadlock detection, the point of this is to make the randomizer filter out the moves that will cause the boxes to be unmovable)

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,9 +1,15 @@
 package solver;
+import java.util.Random;
 
 public class SokoBot {
-    String output;
-  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
+    String output = "";
+    private char[][] mapData;
+    private char[][] itemsData;
+
+    public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+        this.mapData = mapData;
+        this.itemsData = itemsData;
+      /*
      * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
      */
     /*
@@ -12,35 +18,71 @@ public class SokoBot {
      * that just moves left and right repeatedly.
      */
     try {
-      Thread.sleep(3000);
+      Thread.sleep(200);
     } catch (Exception ex) {
       ex.printStackTrace();
     }
 
-    right();
-    right();
-    right();
+    showPlayerCoordinate();
+
+    Runnable[] moves = {
+            () -> up(),
+            () -> down(),
+            () -> left(),
+            () -> right()
+    };
+
+    // THIS IS TEMPORARY SOLUTION
+    // THIS IS TO TEST THE DEADLOCK
+
+    Random rand = new Random();
+    while (output.length() < 1000) {
+        int num = rand.nextInt(4);
+        moves[num].run();
+    }
 
     return output;
   }
 
-  public void up(){
-      output += "u";
+  public void showPlayerCoordinate(){
+      for(int i = 0; i < mapData.length; i++) {
+          for(int j = 0; j < mapData[0].length; j++) {
+              if(mapData[i][j] == '.') {
+                  System.out.print("Goal: (" + j + "," + i + ")\n");
+              }
+          }
+      }
+
+      for(int i = 0; i < itemsData.length; i++) {
+          for(int j = 0; j < itemsData[0].length; j++) {
+              if(itemsData[i][j] == '@' ) {
+                  System.out.print("Player: (" + j + "," + i + ")\n");
+              }
+              if(itemsData[i][j] == '$' ) {
+                  System.out.print("Crates: (" + j + "," + i + ")\n");
+              }
+          }
+      }
   }
 
-  public void down(){
-      output += "d";
-  }
+    public void up(){
+        output += "u";
+    }
 
-  public void left(){
-      output += "l";
-  }
+    public void down(){
+        output += "d";
+    }
 
-  public void right(){
-      output += "r";
-  }
+    public void left(){
+        output += "l";
+    }
 
+    public void right(){
+        output += "r";
+    }
 
-
+    public void deadlockDetection(){
+        // implement your code here
+    }
 
 }
