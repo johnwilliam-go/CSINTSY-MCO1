b6603da829e3b464dadaa8dab43c4cd6ca0c5f21
zaccheusjosecalderon-del <zaccheus_jose_calderon@dlsu.edu.ph>
2026-06-23 22:33:12 +0800

    for getting individ game state for easir bfs implement

---
+++ src/solver/State.java
@@ -0,0 +1,31 @@
+package solver;
+import java.util.*;
+
+public class State {
+    private int playerX, playerY;
+    private String path;
+    private Set<String> cratePositions;
+
+    public State(int playerX, int playerY, String path, Set<String> cratePositions){
+        this.playerX = playerX;
+        this.playerY = playerY;
+        this.cratePositions = cratePositions;
+        this.path = path;
+    }
+
+    // This is for handling individual coordinates separately
+    // Crucial for checking whether two game states are equal.
+    // For checking visited paths of the player.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(instanceof SokobanState)){
+            return false;
+        }
+        State thatOne = (State) o;
+        return playerX == thatOne.playerX && playerY == thatOne.playerY && Objects.equals(cratePositions, thatOne.cratePositions);
+    }
+}
